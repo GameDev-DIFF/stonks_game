e01d1fd77cdd3ca1b2c2ba23c094651b352ae25d
PS220125 <74543772+PS220125@users.noreply.github.com>
2022-11-04 13:01:47 +0100

    Feature/basic character (#24)
    * Added CharacterTest to the scenes
    
    * .vscodefile deleted
    
    * Added basic player controller and health script
    
    Co-authored-by: Mylo Rongen <PS220125@summacollege.nl>

---
+++ Assets/Character/Health.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
+    [SerializeField] private Transform player;
+
+    public float currentHealth { get; private set; }
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    // Is called everytime te player gets damage
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            StartCoroutine(Invunerability());
+        }
+        else
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(9, 10, true);
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+
+        Physics2D.IgnoreLayerCollision(9, 10, false);
+    }
+}
---
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalInput;
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves the player accross the horizontal axis.
+        horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+        // Checks if the player wants to jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        // Chances direction of player
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void Jump()
+    {
+        // Gets the velocity from the rigidbody for the x axis and adds an amount to the y axis.
+        if (isGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+}
