38c46190f0d76b65ccceccff45e7175bb96b2a22
Jens Ramakers <jensramakers@gmail.com>
2022-11-09 22:11:45 +0100

    feature/camera-movement: implemented new camera follow/movement script

--- Assets/Character/Scripts/CameraMovement.cs
+++ Assets/Character/Scripts/CameraMovement.cs
@@ -1,25 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private Transform player;
-    [SerializeField] private float aheadDistance;
-    [SerializeField] private float cameraSpeed;
-    private float lookAhead;
+    private float SmoothValue = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    private bool isHigh;
+    private PlayerMovement playerMovement;
+    
+    [SerializeField] private Transform target;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-
+        playerMovement = target.gameObject.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        float y = 3.25f;
+        switch (target.position.y)
+        {
+            case > 3f:
+            {
+                if (playerMovement.IsGrounded())
+                {
+                    isHigh = true;
+                }
+
+                if (isHigh)
+                {
+                    y = target.position.y;
+                }
+
+                break;
+            }
+            case < -0.2f:
+                y = target.position.y + 2f;
+                isHigh = false;
+                break;
+            default:
+                isHigh = false;
+                break;
+        }
+
+        Vector3 targetPosition = new Vector3(target.position.x, y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothValue);
     }
 }
