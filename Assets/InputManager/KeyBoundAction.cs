0c30583acbc1f6093de4115c4c0c634d3fc4005b
Ralf de Nijs <74543823+PS219809@users.noreply.github.com>
2022-11-09 13:46:38 +0100

    Removed KeyBindings and KeyBindingCheck, Added Axis and KeyBoundAction (#28)
    * Added InputManager, KeyBindings and KeyBindingAction
    
    * Removed KeyBindings and KeyBindingCheck, Added Axis and KeyBoundAction
    
    Co-authored-by: PS220125 <74543772+PS220125@users.noreply.github.com>

---
+++ Assets/InputManager/KeyBoundAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class KeyBoundAction
+{
+    public string Name;
+    public KeyCode Button = KeyCode.None;
+    public KeyCode AltButton = KeyCode.None;
+}
