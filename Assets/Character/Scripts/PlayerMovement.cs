a924a31351e57f6f962dbf26ddb569501623e5e7
PS220125 <74543772+PS220125@users.noreply.github.com>
2022-11-11 09:11:04 +0100

    Feature/basic character (#30)
    * Added CharacterTest to the scenes
    
    * .vscodefile deleted
    
    * Added basic player controller and health script
    
    * horizontalinput check
    
    * made changes the movement
    
    * Changed jumping value, added custom control, made a simple example scene
    
    * feature/basic_character: removed Debug.log
    
    * nothing
    
    * Player can perform crouch and can climb ladders
    
    * changed variable isgrounded, it was incorrect before
    
    Co-authored-by: Mylo Rongen <PS220125@summacollege.nl>
    Co-authored-by: Jens Ramakers <jensramakers@gmail.com>

--- Assets/Character/Scripts/PlayerMovement.cs
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -5,60 +5,101 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontalInput;
+    private float verticalInput;
     private Rigidbody2D rb;
     [SerializeField] private float speed;
+    [SerializeField] private float distance;
     [SerializeField] private float jumpPower;
-    private BoxCollider2D boxCollider;
+    [SerializeField] private BoxCollider2D standingCollider, crouchingCollider;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask whatIsLadder;
+    [SerializeField] private Transform playerForm;
+    private bool isCrouching = false;
+    private bool isClimbing;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, 0);
-        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Moves the player accross the horizontal axis.
-        /*        horizontalInput = InputManager.Instance.GetMoveAxis();*/
-
-        horizontalInput = Input.GetAxis("Horizontal");
-
+        horizontalInput = InputManager.Instance.GetAxis("Horizontal");
         if (horizontalInput != 0)
             rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
 
+        // Ladder climbing
+        climbing();
+
         // Checks if the player wants to jump
-        if (InputManager.Instance.GetKey(KeyBindingAction.Jump))
+        if (InputManager.Instance.GetKey("Jump"))
         {
             Jump();
         }
 
+        // Checks if the player wants to crouch
+        if (InputManager.Instance.GetKey("Crouch"))
+        {
+            Crouch();
+        }
+        else
+        {
+            playerForm.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
         // Chances direction of player
-        if (horizontalInput > 0.01f)
+/*        if (horizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
         }
         else if (horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
-        }
+        }*/
     }
 
     private void Jump()
     {
         // Gets the velocity from the rigidbody for the x axis and adds an amount to the y axis.
-        if (IsGrounded())
+        if (isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
         }
     }
 
-    public bool IsGrounded()
+    private void Crouch()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        playerForm.transform.localScale = new Vector3(1f, 0.5f, 1f);
+    }
+
+    private void climbing()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+
+        if (hitInfo.collider != null)
+            isClimbing = true;
+        else
+            isClimbing = false;
+
+        if (isClimbing == true)
+        {
+            verticalInput = InputManager.Instance.GetAxis("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, verticalInput * speed);
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    public bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(standingCollider.bounds.center, standingCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
 }
