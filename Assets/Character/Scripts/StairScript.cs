267f1d4f467b3fe49a414f752a3fc0b777b07e08
Jens Ramakers <jensramakers@gmail.com>
2022-11-18 18:30:59 +0100

    feature/ball-dispenser-2: stairs fixed

--- Assets/Character/Scripts/StairScript.cs
+++ Assets/Character/Scripts/StairScript.cs
@@ -1,17 +1,20 @@
+using System;
 using UnityEngine;
 
 namespace Character.Scripts
 {
     public class StairScript : MonoBehaviour
-    {
-        [SerializeField] private bool isHigh;
-        [SerializeField] private Transform target;
- 
+    { 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.gameObject.CompareTag("Player"))
+            switch (GetComponent<Collider2D>().tag)
             {
-                transform.parent.GetComponent<Collider2D>().enabled = isHigh;
+                case "UnderneathStair":
+                    Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(), col, true);
+                    break;
+                case "AboveStair":
+                    Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(), col, false);
+                    break;
             }
         }
     }
