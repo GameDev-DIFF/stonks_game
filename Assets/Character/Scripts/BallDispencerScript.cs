85387f8648fb98c3a4a43bdfcd47ae101bc674a1
Jens Ramakers <jensramakers@gmail.com>
2022-11-18 11:44:14 +0100

    feature/ball-dispenser: disposeball

--- Assets/Character/Scripts/BallDispencerScript.cs
+++ Assets/Character/Scripts/BallDispencerScript.cs
@@ -1,36 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallDispencerScript : MonoBehaviour
 {
-    [SerializeField] private GameObject ball;
-    private BallDispencerScript ballDispencer;
     private int seconds;
-    private GameObject ballInstance;
+    private BallDispencerScript ballDispencer;
+    [SerializeField] private GameObject[] ballInstance;
+    private int ballCount;
 
-    void Start()
+    private void Start()
     {
         ballDispencer = gameObject.GetComponent<BallDispencerScript>();
-
-        ballInstance = Instantiate(ball, ballDispencer.transform.position, Quaternion.identity);
-        ballInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(-10f,0);
     }
 
     private void LateUpdate()
     {
-        var dinges = ballInstance.GetComponent<Rigidbody2D>().velocity;
-        ballInstance.GetComponent<Rigidbody2D>().velocity = 5f * dinges.normalized;
+        foreach (var t in ballInstance)
+        {
+            var dinges = t.GetComponent<Rigidbody2D>().velocity;
+            t.GetComponent<Rigidbody2D>().velocity = 5f * dinges.normalized;
+        }
     }
 
     void Update()
     {
-        // if (Time.time > seconds)
-        // {
-        //     seconds += 2;
-        //     GameObject ballInstance = Instantiate(ball, ballDispencer.transform.position, Quaternion.identity);
-        //     ballInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(-10f,0);
-        // }
+        if (Time.time > seconds && ballCount < 3)
+        {
+            ballInstance[ballCount].SetActive(true);
+            ballInstance[ballCount].transform.position = ballDispencer.transform.position;
+            ballInstance[ballCount].GetComponent<Rigidbody2D>().velocity = new Vector2(-10f, 0);
+            
+            seconds += 5;
+            ballCount++;
+        }
     }
 }
