5c122885ddb91efe9e8e68418ab4951372fa4f40
PS198115 <ps198115@edu.summacollege.nl>
2022-12-21 15:29:11 +0100

    Trainlevel traps and enemies have been added in most arease

---
+++ .history/Assets/Trap/Damage object/RedSquare_20221216090030.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedSquare : MonoBehaviour
+{
+    [SerializeField] protected float damage;
+
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Health>()?.TakeDamage(damage);
+
+            var force = transform.position - collision.transform.position;
+            force.Normalize();
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(-force.x, -force.y, force.z) * 10, ForceMode2D.Impulse);
+        }
+    }
+}
